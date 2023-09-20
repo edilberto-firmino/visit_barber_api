1587b76093b6bef37344ef79afb8d97903f88af4
edilberto-firmino <edilberto_ti@yahoo.com.br>
2023-09-20 00:54:51 -0300

    verificar essas rotas com ismael

--- routes/api.php
+++ routes/api.php
@@ -7,8 +7,8 @@ use App\Http\Controllers\ClientController;
 use App\Http\Controllers\AddressController;
 use App\Http\Controllers\CutGalleryController; 
 use App\Http\Controllers\CutsTypeController;
-use App\Http\Controllers\SchedulesController;
-use App\Http\Controllers\OpeningHoursController;
+use App\Http\Controllers\ScheduleController;
+use App\Http\Controllers\OpeningHourController;
 use App\Http\Controllers\ProfilePictureController;
 use App\Http\Controllers\UserController;
 
@@ -20,9 +20,7 @@ Route::apiResource('client', ClientController::class);
 Route::apiResource('address', AddressController::class);
 Route::apiResource('cutgalery', CutGalleryController::class);
 Route::apiResource('cutstype', CutsTypeController::class);
-Route::apiResource('schedules', SchedulesController::class);
-Route::apiResource('openinghours', OpeningHoursController::class);
 Route::apiResource('profilepicture', ProfilePictureController::class);
 Route::apiResource('users', UserController::class);
-
-
+// Route::apiResource('schedules', ScheduleController::class);
+// Route::apiResource('openinghours', OpeningHourController::class);
